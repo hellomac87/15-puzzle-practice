fcb4f82afb3bf6bb22823baf63c2531b60e3013a
hellomac87 <hellomac87@gmail.com>
2018-10-26 15:18:02 +0900

    Feat: move empty cell

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
 const state = {
   puzzle: [
-    [1,2,3,4],
-    [5,null,7,8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-  ]
-}
-function init(){
-  draw();
-}
-// draw puzzle
-function draw(){
-  document.querySelectorAll('.row').forEach((rowEl, rowIndex) => {
-    rowEl.querySelectorAll('.col').forEach((colEl, colIndex) => {
-      colEl.textContent = state.puzzle[rowIndex][colIndex];
-    });
-  });
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,10,11],
+    [12,13,14,15]
+  ],
+  positon: {
+    x: 0,
+    y: 0
+  }
 }
 
+// update puzzle state
+document.querySelectorAll('.row').forEach((row, rowIndex) => {
+  row.querySelectorAll('.col').forEach((col, colIndex) => {
+    col.addEventListener('click', (e) => {
+      state.puzzle[state.positon.x][state.positon.y] = col.textContent;
+      state.puzzle[rowIndex][colIndex] = 0;
 
-// event handler
-document.querySelectorAll('.row').forEach((rowEl, rowIndex) => {
-  rowEl.querySelectorAll('.col').forEach((colEl, colIndex) => {
-    colEl.addEventListener('click', (e) => {
-      // colEl.textContent = null;
-      // state.puzzle[rowIndex][colIndex] = null;
-      console.log('rowIndex: ', state.puzzle[rowIndex]);
-      console.log('colIndex: ', state.puzzle[colIndex]);
-
-      draw();
+      state.positon = {
+        x: rowIndex,
+        y: colIndex
+      }
+      // 업데이트 된 상태를 그린다.
+      drawBoard();
     });
   });
 });
 
+function drawBoard(){
+  // draw puzzle
+  document.querySelectorAll('.row').forEach((row, rowIndex) => {
+    row.querySelectorAll('.col').forEach((col, colIndex) => {
+      col.textContent = state.puzzle[rowIndex][colIndex];
+    });
+  });
+}
 
-init();
+
+
+
+drawBoard()
